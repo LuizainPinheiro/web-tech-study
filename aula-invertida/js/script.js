8390a1327d3b65526a7087ef2b211bc81daf6b94
Luiza <luiza12pinheiro@gmail.com>
2026-06-22 11:04:03 -0300

    feat: sumir e mostrar input

--- aula-invertida/js/script.js
+++ aula-invertida/js/script.js
@@ -1,11 +1,31 @@
-let inputProduto = document.querySelector("#Produto")
+let inputProduto = document.querySelector("#produto")
 let btnForm = document.querySelector("#btn-form")
 let lista = document.querySelector("#lista")
+const btnSumir = document.querySelector("#btn-sumir")
+const btnMostrar = document.querySelector("#btn-mostrar")
 
 btnForm.addEventListener("click", (e) => {
     e.preventDefault();
 
     alert("Funcionou!")
+
+    let valorLista = inputProduto.value
+    console.log(valorLista)
+
+    if(valorLista == ""){
+        alert("Preencher campo!")
+        return  false
+    }
 })
 
+btnSumir.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputProduto.classList.add("sumir-input")
+    inputProduto.classList.remove("mostrar-input")
+})
 
+btnMostrar.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputProduto.classList.add("mostrar-input")
+    inputProduto.classList.remove("sumir-input")
+})
