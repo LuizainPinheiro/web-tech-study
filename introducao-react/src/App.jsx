97d0657029a4370fe2de8608078686e826030d0f
Luiza <luiza12pinheiro@gmail.com>
2026-08-12 10:52:48 -0300

    introdução react

---
+++ introducao-react/src/App.jsx
@@ -0,0 +1,26 @@
+import Header from './Components/Header'
+import Card from './Components/Card' 
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <Card />
+
+      <Card />
+
+      <Card />
+
+  
+    
+    <footer>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, neque!</p>   
+     </footer>
+
+     </>
+  )
+}
+
+export default App
---
+++ introducao-react/src/Components/Card.jsx
@@ -0,0 +1,18 @@
+//Importe da imagem
+import imgCard from "../assets/img-card.jpg";
+
+const Card = () => {
+
+
+return (
+        <section>
+            <article className="card">
+                <img src={imgCard} className="img-card" />
+                <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, neque!</p>
+            </article>
+            <hr />
+        </section>
+    )
+}
+
+export default Card
